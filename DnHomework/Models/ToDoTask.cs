4bbd8b65089c87ecc947a1037656b4e6d0d86f2b
olgamezhova <o.mezhova@dodopizza.com>
2024-10-19 18:15:41 +0400

    First commit

---
+++ DnHomework/Models/ToDoTask.cs
@@ -0,0 +1,12 @@
+namespace DnHomework.Models;
+
+public class ToDoTask
+{
+    public int Id { get; set; }
+    public string Header { get; set; }
+    public string? Description { get; set; }
+    public DateTime CreatedDateTime { get; set; }
+    public DateTime? ModifiedDateTime { get; set; }
+
+    public override string ToString() => $"ToDoTask.Id = {Id.ToString()}";
+}
